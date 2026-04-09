0041bf4ed4f7a3bafe2226c7e29e96f18da32d2c
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-04-09 21:43:59 +0530

    create AuthController class

---
+++ backend/src/main/java/com/inkmatch/backend/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.inkmatch.backend.controller;
+
+import com.inkmatch.backend.dto.*;
+import com.inkmatch.backend.dto.request.LoginRequest;
+import com.inkmatch.backend.dto.request.RegisterRequest;
+import com.inkmatch.backend.dto.response.AuthResponse;
+import com.inkmatch.backend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody RegisterRequest request) {
+        return new AuthResponse(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody LoginRequest request) {
+        return new AuthResponse(authService.login(request));
+    }
+}
