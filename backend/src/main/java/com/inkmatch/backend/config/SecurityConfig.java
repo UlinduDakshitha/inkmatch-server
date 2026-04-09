89cf81b09e2b258f9c5ec04888b466423e1c4d44
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-04-09 20:36:50 +0530

    update security config classes

--- backend/src/main/java/com/inkmatch/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/inkmatch/backend/config/SecurityConfig.java
@@ -1,22 +1,29 @@
 package com.inkmatch.backend.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import com.inkmatch.backend.security.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.*;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
