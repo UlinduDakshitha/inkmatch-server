dadd1642184c7964ea9cc12f9d5b85303a445461
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-04-12 15:00:28 +0530

    create repository  class

---
+++ backend/src/main/java/com/inkmatch/backend/repository/PortfolioImageRepository.java
@@ -0,0 +1,7 @@
+package com.inkmatch.backend.repository;
+
+import com.inkmatch.backend.entity.PortfolioImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PortfolioImageRepository extends JpaRepository<PortfolioImage, Long> {
+}
