ecccd7460738e11a505bd90e831ec215acec01f4
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-04-13 09:30:01 +0530

    create service  class

---
+++ backend/src/main/java/com/inkmatch/backend/service/StudioService.java
@@ -0,0 +1,27 @@
+package com.inkmatch.backend.service;
+
+import com.inkmatch.backend.entity.Studio;
+import com.inkmatch.backend.entity.User;
+import com.inkmatch.backend.repository.StudioRepository;
+import com.inkmatch.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudioService {
+
+    private final StudioRepository studioRepository;
+    private final UserRepository userRepository;
+
+    public Studio createStudio(Long ownerId, Studio studio) {
+
+        User owner = userRepository.findById(ownerId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        studio.setOwner(owner);
+        studio.setVerified(false);
+
+        return studioRepository.save(studio);
+    }
+}
