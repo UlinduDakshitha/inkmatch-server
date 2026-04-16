8e9f80b0cf27925751fc9a5694da0e4b18abda81
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-04-16 16:46:58 +0530

    update

--- backend/src/main/java/com/inkmatch/backend/entity/PortfolioImage.java
+++ backend/src/main/java/com/inkmatch/backend/entity/PortfolioImage.java
@@ -1,23 +1,13 @@
 package com.inkmatch.backend.entity;
-
 import jakarta.persistence.*;
 import lombok.*;
-
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class PortfolioImage {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
-    @ManyToOne
-    private Portfolio portfolio;
-
+    private Long portfolioId;
     private String imageUrl;
-    private String publicId;
 }
--- backend/src/main/java/com/inkmatch/backend/repository/PortfolioImageRepository.java
+++ backend/src/main/java/com/inkmatch/backend/repository/PortfolioImageRepository.java
@@ -3,5 +3,6 @@ package com.inkmatch.backend.repository;
 import com.inkmatch.backend.entity.PortfolioImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PortfolioImageRepository extends JpaRepository<PortfolioImage, Long> {
+public interface PortfolioImageRepository
+        extends JpaRepository<PortfolioImage, Long> {
 }
