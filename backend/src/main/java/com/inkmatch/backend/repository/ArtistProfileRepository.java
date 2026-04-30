84badf10c8ebd5797796d285568dbbf41625908f
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-04-30 20:12:37 +0530

    update

--- backend/src/main/java/com/inkmatch/backend/repository/ArtistProfileRepository.java
+++ backend/src/main/java/com/inkmatch/backend/repository/ArtistProfileRepository.java
@@ -2,7 +2,6 @@ package com.inkmatch.backend.repository;
 
 import com.inkmatch.backend.entity.ArtistProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
