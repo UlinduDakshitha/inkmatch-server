f79879edcd080f69bec92cf70c0aaad1d6957d29
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-05-02 21:26:22 +0530

    update

--- backend/src/main/java/com/inkmatch/backend/controller/AdminController.java
+++ backend/src/main/java/com/inkmatch/backend/controller/AdminController.java
@@ -1,10 +1,9 @@
 package com.inkmatch.backend.controller;
 import com.inkmatch.backend.service.AdminService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -16,5 +15,18 @@ public class AdminController {
     @PutMapping("/verify/{userId}")
     public void verify(@PathVariable Long userId){
         adminService.verifyArtist(userId);
+    }
+
+    @GetMapping("/stats")
+    public Map<String, Object> stats() {
+        return adminService.getStats();
+    }
+
+    @PutMapping("/verification/{id}")
+    public void verify(
+            @PathVariable Long id,
+            @RequestParam String status
+    ) {
+        adminService.updateVerification(id, status);
     }
 }
