6c85b422e985840a71773ad100ca9044caca2de8
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-05-18 23:00:08 +0530

    update

--- backend/src/main/java/com/inkmatch/backend/controller/ArtistController.java
+++ backend/src/main/java/com/inkmatch/backend/controller/ArtistController.java
@@ -15,7 +15,6 @@ public class ArtistController {
 
     private final ArtistService artistService;
 
-
     @PostMapping("/{userId}")
     public ArtistProfile create(@PathVariable Long userId,
                                 @RequestBody ArtistProfileDTO dto) {
