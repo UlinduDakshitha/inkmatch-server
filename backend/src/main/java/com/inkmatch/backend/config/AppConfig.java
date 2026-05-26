eac9e90284a14f20e1fb915e726ab4f9c613922e
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-05-26 20:56:43 +0530

    update

--- backend/src/main/java/com/inkmatch/backend/config/AppConfig.java
+++ backend/src/main/java/com/inkmatch/backend/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
